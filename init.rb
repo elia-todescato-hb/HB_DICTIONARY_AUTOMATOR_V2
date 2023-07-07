eac6e84f22fc6df10deb792ea70492d90dd3ff6b
elia-todescato-hb <elia.todescato@hbenchmark.it>
2023-07-07 15:53:46 +0200

    aggiunto controllo della cartella frontend in init.rb

--- init.rb
+++ init.rb
@@ -2,26 +2,40 @@ require_relative "./tools/gem/install_gem.rb"
 require 'colorize'
 require 'open3'
 
+
+if File.directory?("frontend") == true #check if frontend directory exists
+  puts "Frontend is already installed".blue
+else
+  puts "frontend directory does not exist!".red
+  puts "Install frontend folder? [y/n] ".yellow
+  option = gets
+  if option.chomp.upcase =='Y'
+    `git clone git@github.com:hbenchmark/frontend.git`
+  end
+end
+
+
 # Esegui il comando e ottieni l'output e il valore di ritorno
 puts "Executing command: php -S localhost:8001 -t ./".blue
 command = 'php -S localhost:8001 -t ./'
 output, status = Open3.capture2(command)
 
 
-if status.exitstatus == 1
-  puts "ports already in use would you like another ports? [y/n] ".yellow
-  option = gets
 
-  if option.chomp.upcase =='Y'
-    puts "trying again"
 
-    (8001..8100).each do |port|
-      command = "php -S localhost:#{port} -t ./"
-      output, status = Open3.capture2(command)
-      if status.exitstatus == 1
-        puts "port #{port} is in use".yellow
+  if status.exitstatus == 1
+    puts "ports already in use would you like another ports? [y/n] ".yellow
+    option = gets
+
+    if option.chomp.upcase =='Y'
+      puts "trying again"
+
+      (8001..8100).each do |port|
+        command = "php -S localhost:#{port} -t ./"
+        output, status = Open3.capture2(command)
+        if status.exitstatus == 1
+          puts "port #{port} is in use".yellow
+        end
       end
     end
   end
-end
-
--- src/read_svelte.php
+++ src/read_svelte.php
@@ -1,74 +1,5 @@
 <?php
 
-
-// class read_svelte
-// {
-//   public $svelte_file;
-//   public $dic = "frontend"; //change this variable to change the reader of the dictionary folder
-//   public $nome_dizionario = "dizionario_gestione_strutture"; //TODO: da cambiare come argomento di input
-//   public function __construct()
-//   {
-//     $this->svelte_file = array();
-//   }
-
-
-//   /**
-//    * Get the path of every dictionary file
-//    * @params none
-//    * @return array
-//    */
-//   function getPath(){
-//     $dir_path = $this->dic;
-
-//     //enter in the dir_path and scan all the files that are in the folder
-//     $dir_handle = opendir($dir_path); //open the current directory where the dictionary files are stored
-//     while (false!== ($file = readdir($dir_handle))) { //if directory exists and php can read it
-//       if ($file!= "." && $file!= "..") { //exclude parent directory
-//         $file_info = pathinfo($file);
-//         if ($file_info['extension'] == "svelte") {
-//           $file_path = $dir_path. "/". $file; //get file path
-//           if (is_dir($file_path)) {
-//             echo $this->getPath($file_path); //recursive research
-//           } else {
-//             //push filepath in an array
-//             array_push($this->svelte_file, $file_path);
-//           }
-//         }
-//       }
-//     }
-//     closedir($dir_handle);
-//     return $this->svelte_file; //return an array
-//   }
-
-//   /**
-//    * Read the dictionary file
-//    * @params none
-//   *
-//    */
-//   function read(){
-//     $path = $this->getPath(); //get the path of every dictionary file
-//     foreach ($path as $key => $value) {
-//       $this->svelte_file[$key] = file_get_contents($value);
-//     }
-//     return
-//       $this->clean($this->svelte_file[0]
-//     ); //remove bad char from encoding
-//   }
-
-//   /**
-//    * just clean the current file
-//    * @params string
-//    * @return string
-//    */
-//   function clean($str){
-//     $str_array = explode("const $this->nome_dizionario =", $str); //remove const var =
-//     $str_array = explode("</script>", $str_array[1]); //remove const var =
-//     // preprint($str_array,1);
-//     return $str_array[0];
-//   }
-//   //test
-// }
-
 class read_svelte
 {
   public $svelte_file;
@@ -92,7 +23,7 @@ class read_svelte
     $array_of_dir = $folder_scan->scan($this->dic);
     info("<h1>Searching for svelte file</h1>");
     info("Scanning...");
-    foreach ($array_of_dir as $key => $dir_path) { //ciuclo tutti i percorsi
+    foreach ($array_of_dir as $key => $dir_path) { //ciclo tutti i percorsi
       $folder = pathinfo($dir_path);
       if (key_exists('extension', $folder)) { //allora stiamo valutando un file
         //open dir path that i have
@@ -114,9 +45,8 @@ class read_svelte
     $path = $this->getPath(); //get the path of every dictionary file
     foreach ($path as $key => $value) {
       $this->svelte_file[$key] = file_get_contents($value); //TODO: cambia con value
+
     }
-    // info("sto pulendo:" . count($this->svelte_file) . " dizionari");
-    // info("prima di pulire..");
     return $this->clean($this->svelte_file); //remove bad char from encoding
   }
 
@@ -150,12 +80,6 @@ class read_svelte
       }
       info("finito di pulire..");
     }
-    $fp = fopen("log.txt", 'w');
-    foreach ($return_str as $key => $value) {
-      # code...
-      fwrite($fp, $value);
-    }
-    fclose($fp);
 
     return ($return_str);
   }
