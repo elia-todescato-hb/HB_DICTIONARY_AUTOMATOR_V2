6abc82c64be5cc321b55e5db1e9f102ab886d912
Elia Todescato <eliatodescato@Elias-MacBook-Pro.local>
2023-07-07 21:37:52 +0200

    pulizia generale

--- tools/log/log.php
+++ tools/log/log.php
@@ -3,20 +3,17 @@
 function warning($string = "")
 {
     $warning = 'yellow';
-    # code...
     echo "<pre style='color: {$warning}'><p>{$string}</p></pre>";
 }
 
 function error($string = "")
 {
     $warning = 'red';
-    # code...
     echo "<pre style='color: {$warning}'><p>{$string}</p></pre>";
 }
 
 function info($string = "")
 {
     $warning = 'blue';
-    # code...
     echo "<pre style='color: {$warning}'><p>{$string}</p></pre>";
 }
